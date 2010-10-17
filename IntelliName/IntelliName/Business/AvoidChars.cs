9225c74dbaeea96306ede5d00f52d29102af73e4
Wang Anyu <wang.anyu@gmail.com>
2010-10-17 11:04:46 +0800

    add void chars

---
+++ IntelliName/IntelliName/Business/AvoidChars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntelliName.Business
+{
+    class AvoidChars
+    {
+        public bool IsAvoidChars(char c1)
+        {
+            return false;
+        }
+
+        char[] AvoidChars
+        {
+            get;
+            set;
+        }
+    }
+}
