a64a11d48a6d7f25801c939ee00f3d0f98ee40dc
Wang Anyu <wang.anyu@gmail.com>
2011-01-22 21:59:29 +0800

    Added Moving average calc. (Not completed)

---
+++ Analyzer2/StockAnalyzer/Strategy/Indicator/MovingAvgCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinanceAnalyzer.Strategy.Indicator
+{
+    class MovingAvgCalc : IIndicatorCalc
+    {
+        public string Name
+        {
+            get { return "MA"; }
+        }
+
+        public void Calc(IStockHistory hist)
+        {
+
+        }
+
+        public OperType MatchSignal(DateTime dt, DateTime prev)
+        {
+            return OperType.NoOper;
+        }
+    }
+}
