3f9e3f1ccc1b2169bcc58ddf2907b4faae37c550
Wang Anyu <wang.anyu@gmail.com>
2011-01-23 20:48:52 +0800

    Added moving average prediction

--- Analyzer2/StockAnalyzer/Strategy/Indicator/MovingAvgCalc.cs
+++ Analyzer2/StockAnalyzer/Strategy/Indicator/MovingAvgCalc.cs
@@ -2,24 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FinanceAnalyzer.DB;
+using System.Diagnostics;
+using FinanceAnalyzer.Utility;
 
 namespace FinanceAnalyzer.Strategy.Indicator
 {
-    class MovingAvgCalc : IIndicatorCalc
+    class MovingAvgCalc : HistoricalValuesCalc
     {
-        public string Name
+        public MovingAvgCalc()
+        {
+            _Prediction.LongDays = 10;
+            _Prediction.ShortDays = 5;
+        }
+
+        public override string Name
         {
             get { return "MA"; }
         }
 
-        public void Calc(IStockHistory hist)
+        public override void Calc(IStockHistory hist)
         {
+            _History = hist;
 
+            DateTime startDate = hist.MinDate;
+            DateTime endDate = hist.MaxDate;
+
+            while (startDate < endDate)
+            {
+                IStockData stock = hist.GetStock(startDate);
+                if (stock == null)
+                {
+                    startDate = DateFunc.GetNextWorkday(startDate);
+                    continue;
+                }
+
+                _Prediction.AddPrice(stock.EndPrice);
+
+                if (!_Prediction.IsCountEnough())
+                {
+                    startDate = DateFunc.GetNextWorkday(startDate);
+                    continue;
+                }
+
+                // 长周期日线和短周期日线
+                double longEMA = _Prediction.GetLongAverage();
+                double shortEMA = _Prediction.GetShortAverage();
+
+                double diff = shortEMA - longEMA; // 短周期均值-长周期均值
+
+                _DateIndicators.Add(startDate, diff);
+
+                startDate = DateFunc.GetNextWorkday(startDate);
+            }
         }
 
-        public OperType MatchSignal(DateTime dt, DateTime prev)
+        public override OperType MatchSignal(DateTime dt, DateTime prev)
         {
-            return OperType.NoOper;
+            if (double.IsNaN(GetIndicatorValue(dt)) || double.IsNaN(GetIndicatorValue(prev)))
+            {
+                return OperType.NoOper;
+            }
+
+            if (this.GetIndicatorValue(prev) < 0)
+            {
+                return OperType.Buy;
+            }
+            else if (this.GetIndicatorValue(prev) > 0)
+            {
+                return OperType.Sell;
+            }
+            else
+            {
+                return OperType.NoOper;
+            }
         }
+
+        IStockHistory _History;
+        MovingAveragePrediction _Prediction = new MovingAveragePrediction();
     }
 }
---
+++ Analyzer2/StockAnalyzer/Utility/MovingAveragePrediction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinanceAnalyzer.Utility
+{
+    class MovingAveragePrediction
+    {
+        public int LongDays
+        {
+            get;
+            set;
+        }
+
+        public int ShortDays
+        {
+            get;
+            set;
+        }
+
+        public double CalcNextPredictionValue()
+        {
+            if (!IsCountEnough())
+            {
+                return double.NaN;
+            }
+
+            double curLongAvg = _LongPriceList.Average();
+            double curShortAvg = _ShortPriceList.Average();
+
+            if (curShortAvg > curLongAvg)
+            {
+                double val = (curLongAvg - curShortAvg) * LongDays * ShortDays / (LongDays - ShortDays);
+                return (val + 0.01);
+            }
+            else
+            {
+                double val = (curLongAvg - curShortAvg) * LongDays * ShortDays / (LongDays - ShortDays);
+                return (val - 0.01);
+            }
+        }
+
+        public void AddPrice(double price)
+        {
+            _LongPriceList.Add(price);
+            if (_LongPriceList.Count > LongDays)
+            {
+                _LongPriceList.RemoveAt(0);
+            }
+
+            _ShortPriceList.Add(price);
+            if (_ShortPriceList.Count > ShortDays)
+            {
+                _ShortPriceList.RemoveAt(0);
+            }
+        }
+
+        public bool IsCountEnough()
+        {
+            return (_LongPriceList.Count >= LongDays) && (_ShortPriceList.Count >= ShortDays);
+        }
+
+        public double GetLongAverage()
+        {
+            return _LongPriceList.Average();
+        }
+
+        public double GetShortAverage()
+        {
+            return _ShortPriceList.Average();
+        }
+
+        List<double> _LongPriceList = new List<double>();
+        List<double> _ShortPriceList = new List<double>();
+    }
+}
