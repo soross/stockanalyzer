119058905e45bdad526e036c3b5911b62a3ae00f
Wang Anyu <wang.anyu@gmail.com>
2011-02-15 21:18:52 +0800

    Fix YahooStockServer class

---
+++ Analyzer2/StockAnalyzer/DataAcquisition/StockDataAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FinanceAnalyzer.DB;
+
+namespace FinanceAnalyzer.DataAcquisition
+{
+    class StockDataAdapter
+    {
+        public static IStockData ToStockData(DotNetStock.Engine.Stock stock)
+        {
+            StockData data = new StockData();
+
+            data.Amount = stock.getVolume();
+
+            return data;
+        }
+    }
+}
