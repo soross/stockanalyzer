20118c04dbc614d84cc493ccd92be3f7aa4cd7e1
Wang Anyu <wang.anyu@gmail.com>
2011-09-25 22:38:36 +0800

    Refactor

--- Analyzer2/StockAnalyzer/Tactics/Implementation/TacticsFactory.cs
+++ Analyzer2/StockAnalyzer/Tactics/Implementation/TacticsFactory.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using FinanceAnalyzer.Tactics.StockExpression.Implementation;
 using FinanceAnalyzer.Tactics.StockExpression;
 
 namespace FinanceAnalyzer.Tactics.Implementation
 {
     class TacticsFactory
     {
-        public void Init()
+        public void Init(IStockHistory hist)
         {
-            IStockExpression tac = StockExpressionFactory.Instance().GetExpression("Three Days");
+            IStockExpression expr = StockExpressionFactory.Instance().GetExpression(StockExpressionNames.THREEDAYS);
+            ITactics tac = new CommonTactics();
+            tac.CurrentExpression = expr;
+            tac.History = hist;
 
+            allTactics.Add(tac);
+        }
+
+        public void Run()
+        {
+            foreach (var tac in allTactics)
+            {
+                tac.Run();
+            }
         }
 
         List<ITactics> allTactics = new List<ITactics>();
