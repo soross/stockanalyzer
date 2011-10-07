aa77fa278fdbaa22ed4b71783f5f3e854d6b611d
Wang Anyu <wang.anyu@gmail.com>
2011-10-07 19:54:03 +0800

    Downloader UI

---
+++ Analyzer2/StockDownloader/Log/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StockDownloader.Log
+{
+    interface ILogger
+    {
+        void Log(string info);
+        ListBox UILog
+        {
+            set;
+        }
+    }
+}
---
+++ Analyzer2/StockDownloader/Log/LogManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StockDownloader.Log
+{
+    class LogManager : ILogger
+    {
+        private LogManager()
+        {
+        }
+
+        public static ILogger GetInstance()
+        {
+            return instance_;
+        }
+
+        public void Log(string info)
+        {
+            string datetime = DateTime.Now.ToShortTimeString();
+            LogListBox_.Items.Add(datetime + ": " + info);
+        }
+
+        public ListBox UILog
+        {
+            set
+            {
+                LogListBox_ = value;
+            }
+        }
+
+        ListBox LogListBox_;
+
+        static LogManager instance_ = new LogManager();
+    }
+}
