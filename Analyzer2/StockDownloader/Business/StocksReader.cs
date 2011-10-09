51c71b413b31b454194af969b64b04870c5a1762
Wang Anyu <wang.anyu@gmail.com>
2011-10-09 22:37:24 +0800

    Add StocksReader (not complete)

---
+++ Analyzer2/StockDownloader/Business/StocksReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockDownloader.Business
+{
+    class StocksReader
+    {
+        public void Load()
+        {
+        }
+    }
+}
