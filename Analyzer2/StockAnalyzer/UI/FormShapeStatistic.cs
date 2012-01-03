df1f8750503a5152b044b70b93e98b2563f2fd83
Wang Anyu <wang.anyu@gmail.com>
2012-01-03 23:13:55 +0800

    Added dialog for statistic (up or down rate for a shape or sequence)

---
+++ Analyzer2/StockAnalyzer/UI/FormShapeStatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinanceAnalyzer.UI
+{
+    public partial class FormShapeStatistic : Form
+    {
+        public FormShapeStatistic()
+        {
+            InitializeComponent();
+        }
+    }
+}
