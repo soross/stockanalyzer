762c8bf9139c1c41f0c35adc0672d31b4a7367b9
Wang Anyu <wang.anyu@gmail.com>
2012-01-24 14:31:45 +0800

    Improvement: Save stock to MongoDB

---
+++ Analyzer2/Stock.Db/IO/StockFindHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using Stock.Common.Data;
+
+namespace Stock.Db.IO
+{
+    class StockFindHelper
+    {
+        public void Init(MongoCollection<StockData> coll)
+        {
+            StockIdAndDates_.Clear();
+
+            foreach (var item in coll.FindAll())
+            {
+                AddStockDay(item.StockId, item.TradeDate);
+            }
+        }
+
+        public void AddStockDay(int stockId, DateTime dt)
+        {
+            if (StockIdAndDates_.ContainsKey(stockId))
+            {
+                StockIdAndDates_[stockId].Add(dt);
+            }
+            else
+            {
+                List<DateTime> dates = new List<DateTime>();
+                dates.Add(dt);
+
+                StockIdAndDates_.Add(stockId, dates);
+            }
+        }
+
+        public bool FindStock(int stockId, DateTime dt)
+        {
+            if (!StockIdAndDates_.ContainsKey(stockId))
+            {
+                return false;
+            }
+
+            return StockIdAndDates_[stockId].Contains(dt);
+        }
+
+        public IEnumerable<int> GetAllStockIDs()
+        {
+            return StockIdAndDates_.Keys;
+        }
+
+        Dictionary<int, List<DateTime>> StockIdAndDates_ = new Dictionary<int,List<DateTime>>();
+    }
+}
