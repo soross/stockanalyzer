3427dad6af4eb585491863fa66d21c3689fbecb9
Wang Anyu <wang.anyu@gmail.com>
2012-03-05 21:20:55 +0800

    Trend Revert

---
+++ Analyzer2/StockAnalyzer/Strategy/Indicator/Shape/TrendRevertShapeCalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FinanceAnalyzer.Stock;
+
+namespace FinanceAnalyzer.Strategy.Indicator.Shape
+{
+    /// <summary>
+    /// 试图找出顶点
+    /// </summary>
+    class TrendRevertShapeCalc : BasicIndicatorCalc
+    {
+        public override void Calc(IStockHistory hist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get { return "Vertex"; }
+        }
+
+        static const int SLOPE_DAYS = 7;
+        static const int SLOPE_OFFSET_DAYS = 3;
+    }
+}
