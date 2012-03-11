0e3cbecd3f219649ac4639d4fd19fa9c8b7e9a2c
Wang Anyu <wang.anyu@gmail.com>
2012-03-11 14:37:12 +0800

    refactor

--- Analyzer2/StockAnalyzer/DB/IStockDBReader.cs
+++ Analyzer2/StockAnalyzer/DB/IStockDBReader.cs
@@ -6,6 +6,9 @@ using Stock.Common.Data;
 
 namespace FinanceAnalyzer.DB
 {
+    /// <summary>
+    /// Load all stock information from DB
+    /// </summary>
     interface IStockDBReader
     {
         IEnumerable<StockData> Load(int stockId);
