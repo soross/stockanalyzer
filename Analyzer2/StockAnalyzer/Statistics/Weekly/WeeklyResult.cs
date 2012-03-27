06a83fcb413a14ebb37b01f2c60e00eac59a9339
Wang Anyu <wang.anyu@gmail.com>
2012-03-27 23:37:47 +0800

    Added weekly shape analysis

---
+++ Analyzer2/StockAnalyzer/Statistics/Weekly/WeeklyResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stock.Common.Data;
+
+namespace FinanceAnalyzer.Statistics.Weekly
+{
+    class WeeklyResult
+    {
+        public void AddStockData(IStockData dt)
+        {
+            DayOfWeek dw = dt.TradeDate.DayOfWeek;
+
+            if (dayToStocks_.ContainsKey(dw))
+            {
+                // Do nothing
+            }
+            else
+            {
+                dayToStocks_.Add(dw, dt);
+            }
+        }
+
+        public bool IsWholeWeek()
+        {
+            return dayToStocks_.ContainsKey(DayOfWeek.Monday)
+                && dayToStocks_.ContainsKey(DayOfWeek.Tuesday)
+                && dayToStocks_.ContainsKey(DayOfWeek.Wednesday)
+                && dayToStocks_.ContainsKey(DayOfWeek.Thursday)
+                && dayToStocks_.ContainsKey(DayOfWeek.Friday);
+        }
+
+        public string CalcTemplate()
+        {
+            if (!IsWholeWeek())
+            {
+                return "";
+            }
+
+
+
+            return "";
+        }
+
+        Dictionary<DayOfWeek, IStockData> dayToStocks_ = new Dictionary<DayOfWeek, IStockData>();
+    }
+}
---
+++ Analyzer2/StockAnalyzer/Statistics/Weekly/WeeklyResults.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stock.Common.Data;
+using System.Globalization;
+
+namespace FinanceAnalyzer.Statistics.Weekly
+{
+    class WeeklyResults
+    {
+        public void AddStockData(IStockData dt)
+        {
+            int week = calendar_.GetWeekOfYear(dt.TradeDate, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday);
+
+            if (Results_.ContainsKey(week))
+            {
+                Results_[week].AddStockData(dt);
+            }
+            else
+            {
+                WeeklyResult res = new WeeklyResult();
+                res.AddStockData(dt);
+
+                Results_.Add(week, res);
+            }
+        }
+
+        Dictionary<int, WeeklyResult> Results_ = new Dictionary<int, WeeklyResult>();
+
+        Calendar calendar_ = CultureInfo.CurrentCulture.Calendar;
+    }
+}
