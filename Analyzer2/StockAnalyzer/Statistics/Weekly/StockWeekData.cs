85568e7b90836be940082b5e45075d894f2afd3f
Wang Anyu <wang.anyu@gmail.com>
2012-04-07 00:52:23 +0800

    Finish research on Monday and Friday up ratios

---
+++ Analyzer2/StockAnalyzer/Statistics/Weekly/StockWeekData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stock.Common.Data;
+
+namespace FinanceAnalyzer.Statistics.Weekly
+{
+    class StockWeekData
+    {
+        public void AddStockData(IStockData sd)
+        {
+            if (sd == null)
+            {
+                return;
+            }
+
+            if (sd.EndPrice > sd.StartPrice)
+            {
+                upCount_++;
+            }
+
+            totalCount_++;
+        }
+
+        public double CalcUpPercent()
+        {
+            if (totalCount_ == 0)
+            {
+                throw new ArithmeticException();
+            }
+
+            return (double)upCount_ / totalCount_;
+        }
+
+        public int TotalDays
+        {
+            get
+            {
+                return totalCount_;
+            }
+        }
+
+        int upCount_;
+        int totalCount_;
+
+    }
+}
