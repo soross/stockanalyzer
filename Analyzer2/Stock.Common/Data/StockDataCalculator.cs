b7fcbdee6006a99e371cf9c8debecd620ab6b5a0
Wang Anyu <wang.anyu@gmail.com>
2012-06-06 23:10:43 +0800

    Added Engulfing method

---
+++ Analyzer2/Stock.Common/Data/StockDataCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock.Common.Data
+{
+    public class StockDataCalculator
+    {
+        public static double GetRiseRatio(IStockData sd)
+        {
+            return (sd.EndPrice - sd.StartPrice) / sd.StartPrice;
+        }
+
+        public static bool IsRise(IStockData sd)
+        {
+            return sd.EndPrice > sd.StartPrice;
+        }
+
+        public static bool IsDown(IStockData sd)
+        {
+            return sd.EndPrice < sd.StartPrice;
+        }
+
+        public static bool IsDifferentRiseDown(IStockData sd1, IStockData sd2)
+        {
+            if (IsRise(sd1) && IsDown(sd2))
+            {
+                return true;
+            }
+            else if (IsDown(sd1) && IsRise(sd2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Analyzer2/StockAnalyzer/Strategy/Indicator/FibonacciCalc.cs
+++ Analyzer2/StockAnalyzer/Strategy/Indicator/FibonacciCalc.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FinanceAnalyzer.Stock;
 
 namespace FinanceAnalyzer.Strategy.Indicator
 {
     class FibonacciCalc : BasicIndicatorCalc
     {
-        public override void Calc(Stock.IStockHistory hist)
+        public override void Calc(IStockHistory hist)
         {
             throw new NotImplementedException();
         }
