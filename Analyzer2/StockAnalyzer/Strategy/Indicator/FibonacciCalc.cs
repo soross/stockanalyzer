848a01d3712544acbd13921a9951532a6c8a73e8
Wang Anyu <wang.anyu@gmail.com>
2012-06-06 22:02:52 +0800

    Added Fibonacci (not complete)

---
+++ Analyzer2/StockAnalyzer/Strategy/Indicator/FibonacciCalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinanceAnalyzer.Strategy.Indicator
+{
+    class FibonacciCalc : BasicIndicatorCalc
+    {
+        public override void Calc(Stock.IStockHistory hist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get { return "Fibonacci"; }
+        }
+    }
+}
