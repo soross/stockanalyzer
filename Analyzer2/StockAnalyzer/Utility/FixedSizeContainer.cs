3fa84a0c1a2001a32b98ff7902128e752db4687a
Wang Anyu <wang.anyu@gmail.com>
2012-06-23 15:18:08 +0800

    Refactor

---
+++ Analyzer2/StockAnalyzer/Utility/FixedSizeContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinanceAnalyzer.Utility
+{
+    class FixedSizeContainer<T>
+    {
+        public FixedSizeContainer(int totalCount)
+        {
+            FixedSize_ = totalCount;
+        }
+
+        public bool IsEnough()
+        {
+            return list_.Count == FixedSize_;
+        }
+
+        public void Add(T val)
+        {
+            if (list_.Count >= FixedSize_)
+            {
+                list_.RemoveAt(0);
+            }
+
+            list_.Add(val);
+        }
+
+        public T GetAt(int pos)
+        {
+            return list_[pos];
+        }
+
+        List<T> list_ = new List<T>();
+        int FixedSize_;
+    }
+}
